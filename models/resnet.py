61b8baf3defad16d701cd4e7f59598bd2bf78a38
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-07-27 08:51:37 +0700

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -119,9 +119,9 @@ class FBResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 32, layers[0])
-        self.layer2 = self._make_layer(block, 16, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 8, layers[2], stride=2)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         # self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.last_linear = nn.Linear(8 * block.expansion, num_classes)
 
